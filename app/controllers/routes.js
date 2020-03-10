cf1e8b81362f958735f0a597417eebf2afbb6c8d
Alexandre Houmeau <alexhm@MacBook-Pro-de-Alexandre.local>
2020-03-10 18:20:09 +0100

    feat(core): add path article to routes.js

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -4,11 +4,23 @@ const Show = require('./users/show')
 const Delete = require('./users/delete.js')
 const Update = require('./users/update.js')
 
+// Articles
+const CreateArticle = require('./articles/create')
+const ShowArticle = require('./articles/show')
+const UpdateArticle = require('./articles/update')
+const DeleteArticle = require('./articles/delete')
+
 module.exports = {
   users: {
     Create,
     Show, 
     Delete,
     Update
+  },
+  articles: {
+    CreateArticle,
+    ShowArticle, 
+    UpdateArticle,
+    DeleteArticle
   }
 }
