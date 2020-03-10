e4d3d11dba28da12fbaf9a50c489091e637cdd8e
Alexandre Houmeau <alexhm@MacBook-Pro-de-Alexandre.local>
2020-03-10 16:30:27 +0100

    second commit

---
+++ app/controllers/routes.js
@@ -0,0 +1,14 @@
+// Users
+const Create = require('./users/create.js')
+const Show = require('./users/show.js')
+const Delete = require('./users/delete.js')
+const Update = require('./users/update.js')
+
+module.exports = {
+  users: {
+    Create,
+    Show, 
+    Delete,
+    Update
+  }
+}
