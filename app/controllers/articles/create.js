c1a3d21be57be49af127d5f5910845973da41554
Alexandre Houmeau <alexhm@MacBook-Pro-de-Alexandre.local>
2020-03-10 18:21:09 +0100

    feat(core): add create article path

---
+++ app/controllers/articles/create.js
@@ -0,0 +1,41 @@
+const Articles = require('../../models/article')
+const User = require('../../models/user')
+
+/**
+ * Create
+ * @class
+ */
+class Create {
+  constructor (app, connect) {
+    this.app = app
+    this.ArticleModel = connect.model('Articles', Articles)
+    this.UserModel = connect.model('User', User)
+
+    this.run()
+  }
+  /**
+   * middleware
+   */
+  middleware () {
+    this.app.post('/article/create', async (req, res) => {
+      try {
+        const articleModel = new this.ArticleModel(req.body)
+        await articleModel.generateAdmin(req.headers['token'], this.UserModel)
+        res.status(201).send({articleModel})
+      } catch (err) {
+        res.status(500).json({
+          'code': 500,
+          'message': err
+        })
+      }
+    })
+  }
+  /**
+   * run
+   */
+  run () {
+    this.middleware()
+  }
+}
+
+module.exports = Create
